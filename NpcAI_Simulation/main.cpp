3706aaa3f3deb555b5ced713029950084e37486e
SimJoonTTMD <wesley0940@gmail.com>
2021-03-14 23:58:04 +0900

    20210314

---
+++ NpcAI_Simulation/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct mob {
+	int Health_Point, Defand_Power, Attack_Speed, Avoidancd_power, Attack_power;
+	string Group;
+	/*
+	
+	*/
+	float Rate;
+};
+vector<mob> vector_mob[15]; // N번의 학습
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+
+	return 0;
+}
